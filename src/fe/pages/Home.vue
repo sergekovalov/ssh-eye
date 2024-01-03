48193a45ddc714b1bb5a71289833f72ab7312583
Serhii Kovalov <moiserge.k@gmail.com>
2024-01-03 22:16:51 +0100

    feat: upd css

--- src/fe/pages/Home.vue
+++ src/fe/pages/Home.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="flex "
+  <div class="flex align-items-center justify-content-center h-full">
+    <div>
+      <div class="surface-100 p-1">You don't have any connections yet</div>
+      <div class="flex justify-content-center">
+        <Button label="Add Connection" icon="pi pi-plus" iconPos="right" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  
+  import Button from "primevue/button"
 </script>
